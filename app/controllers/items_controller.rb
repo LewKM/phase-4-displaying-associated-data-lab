311c24224f5898acd5355748a303ff297e3093f0
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-09-30 13:08:50 +0300

    Done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,10 @@
 class ItemsController < ApplicationController
+
+    # Items GET /items returns an array of all items with user info
+
+    def index
+        items = Item.all
+        render json: items, include: :user
+    end
+    
 end
